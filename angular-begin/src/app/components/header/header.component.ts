4a34166966b975a4c19f06cefe55db2f6a035f66
ryan <ryannesh2376@gmail.com>
2025-03-03 12:17:24 +0300

    err

--- angular-begin/src/app/components/header/header.component.ts
+++ angular-begin/src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
