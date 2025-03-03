52e044f1575d584731e12b5d615aea8b0683c409
ryan <ryannesh2376@gmail.com>
2025-03-03 12:35:08 +0300

    header-works

--- angular-begin/src/app/app.component.ts
+++ angular-begin/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [HeaderComponent], // RouterOutlet],
+  // template: `
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
