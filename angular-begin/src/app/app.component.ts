fc61c0985f58120c13f9bba282c9d5e54da73e07
ryan <ryannesh2376@gmail.com>
2025-03-03 11:59:24 +0300

    headerCompo

--- angular-begin/src/app/app.component.ts
+++ angular-begin/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-begin';
+  title: string = 'angular-begin';
 }
+
