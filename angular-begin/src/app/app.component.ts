71b7d5532a52fbee93dc898c915199361563cb4f
ryan <ryannesh2376@gmail.com>
2025-03-18 09:23:32 +0300

    ang1

--- angular-begin/src/app/app.component.ts
+++ angular-begin/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [HeaderComponent], // RouterOutlet],
+  imports: [HeaderComponent,CommonModule], // RouterOutlet],
   // template: `
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'angular-begin';
+  title = 'angular-begin';
 }
 
